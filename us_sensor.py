888b9a142989e7049750d1b005309f434b122505
Ayush <payush548@gmail.com>
2025-11-10 21:15:24 -0500

    initial commit

--- us_sensor.py
+++ us_sensor.py
@@ -22,7 +22,7 @@ class UltrasonicSensor:
 
     def start_monitoring_distance(self):
         #allows to monitor the distance in the background
-        if self.moitor_thread and self.monitor_thread.is_alive():
+        if self.monitor_thread and self.monitor_thread.is_alive():
             return
         self.stop_flag.clear()
         self.monitor_thread = threading.Thread(target=self.monitor_loop, daemon=True)
